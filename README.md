905e85c667a76635ab34ec2f98908ba4bd645920
YC <zzero591619@gmail.com>
2019-08-19 20:45:45 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+## Introduction
+vue admin
---
+++ src/views/dashboard/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="dashboard-container">
+    <h1>Halo Laravel!</h1>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Dashboard',
+  data() {
+    return {
+    }
+  },
+}
+</script>
