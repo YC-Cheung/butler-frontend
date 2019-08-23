d3028aa2a7e07e1fb01ea52f7857aa8fb78e0387
YC <zzero591619@gmail.com>
2019-08-23 20:24:49 +0800

    edit axios

--- src/utils/request.js
+++ src/utils/request.js
@@ -3,6 +3,11 @@ import { MessageBox, Message } from 'element-ui'
 import store from '@/store'
 import { getToken } from '@/utils/auth'
 
+const showError = res => {
+  const { message: msg } = res.data
+  msg && Message.error(msg)
+}
+
 // create an axios instance
 const service = axios.create({
   baseURL: process.env.VUE_APP_BASE_API, // url = base url + request url
@@ -19,7 +24,7 @@ service.interceptors.request.use(
       // let each request carry token
       // ['X-Token'] is a custom headers key
       // please modify it according to the actual situation
-      config.headers['X-Token'] = getToken()
+      config.headers['Authorization'] = getToken()
     }
     return config
   },
@@ -32,53 +37,45 @@ service.interceptors.request.use(
 
 // response interceptor
 service.interceptors.response.use(
-  /**
-   * If you want to get http information such as headers or status
-   * Please return  response => response
-  */
-
-  /**
-   * Determine the request status by custom code
-   * Here is just an example
-   * You can also judge the status by HTTP Status Code
-   */
   response => {
-    const res = response.data
-
-    // if the custom code is not 20000, it is judged as an error.
-    if (res.code !== 20000) {
-      Message({
-        message: res.message || 'Error',
-        type: 'error',
-        duration: 5 * 1000
-      })
-
-      // 50008: Illegal token; 50012: Other clients logged in; 50014: Token expired;
-      if (res.code === 50008 || res.code === 50012 || res.code === 50014) {
-        // to re-login
-        MessageBox.confirm('You have been logged out, you can cancel to stay on this page, or log in again', 'Confirm logout', {
-          confirmButtonText: 'Re-Login',
-          cancelButtonText: 'Cancel',
-          type: 'warning'
-        }).then(() => {
-          store.dispatch('user/resetToken').then(() => {
-            location.reload()
-          })
-        })
-      }
-      return Promise.reject(new Error(res.message || 'Error'))
-    } else {
-      return res
-    }
+    const { data: { data }, status } = response
+    return { data, status }
   },
   error => {
     console.log('err' + error) // for debug
-    Message({
-      message: error.message,
-      type: 'error',
-      duration: 5 * 1000
-    })
-    return Promise.reject(error)
+    const { response: res } = error
+
+    if (res) {
+      switch (res.status) {
+        case 404:
+          Message.error('访问资源不存在')
+          break
+        case 401:
+          Message.error('登录已失效，请重新登录')
+          break
+        case 400:
+          showError(res)
+          break
+        case 403:
+          showError(res)
+          break
+        case 422:
+          Message.error('参数格式错误')
+          break
+        case 429:
+          Message.error('操作太频繁，请稍后再试')
+          break
+        default:
+          Message.error(`服务器异常(code: ${res.status})`)
+          break
+      }
+    } else {
+      if (err instanceof axios.Cancel) {
+        console.log(err.toString())
+      } else {
+        debounceMsg('请求失败')
+      }
+    }
   }
 )
 
